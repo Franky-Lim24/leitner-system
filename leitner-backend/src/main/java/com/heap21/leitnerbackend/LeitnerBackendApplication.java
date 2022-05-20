9bc9b96125842c7d3515f03f8cd8e44a2fc24cbe
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2022-05-20 21:42:56 +0800

    create spring boot and react native project

---
+++ leitner-backend/src/main/java/com/heap21/leitnerbackend/LeitnerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heap21.leitnerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitnerBackendApplication.class, args);
+	}
+
+}
---
+++ leitner-backend/src/test/java/com/heap21/leitnerbackend/LeitnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heap21.leitnerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
