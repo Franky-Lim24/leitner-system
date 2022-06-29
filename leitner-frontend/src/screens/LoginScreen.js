f0b59908493044cbbbc6102e240e7e0cd709e0af
gerald1112 <50981837+gerald1112@users.noreply.github.com>
2022-06-29 15:12:21 +0800

    Creation of LoginScreen

---
+++ leitner-frontend/src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const LoginScreen = () => {
+    return (
+      <Text>LoginScreen</Text>
+    );
+  };
+  
+  export default LoginScreen;
+  
