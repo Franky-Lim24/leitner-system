ae277d513a591191c2c2d156ad845834a5d87dca
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2022-07-04 15:38:34 +0800

    updated box class

--- leitner-backend/src/main/java/com/heap21/leitnerbackend/Box.java
+++ leitner-backend/src/main/java/com/heap21/leitnerbackend/Box.java
@@ -1,9 +1,6 @@
 package com.heap21.leitnerbackend;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Box {
