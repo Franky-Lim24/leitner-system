4db9ebca3d574b656f9fc8057232b8654199d454
gerald1112 <50981837+gerald1112@users.noreply.github.com>
2022-07-05 23:25:13 +0800

    updated navigation

--- leitner-frontend/App.js
+++ leitner-frontend/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import HomeScreen from "./src/screens/HomeScreen.js";
-import AnswerScreen from "./src/screens/AnswerScreen.js";
-import QuestionScreen from "./src/screens/QuestionScreen.js";
+// import AnswerScreen from "./src/screens/AnswerScreen.js";
+// import QuestionScreen from "./src/screens/QuestionScreen.js";
 import ScheduleScreen from "./src/screens/ScheduleScreen.js";
 import SignInScreen from "./src/screens/SignInScreen.js";
 import SignUpScreen from "./src/screens/SignUpScreen.js";
+import SplashScreen from "./src/screens/SplashScreen.js"
+
 
 const Stack  = createStackNavigator();
 
@@ -16,11 +19,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions = {{headerShown:false}}>
+      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Schedule" component={ScheduleScreen} />
-      <Stack.Screen name="FlashCardQuestion" component={QuestionScreen} />
-      <Stack.Screen name="FlashCardAnswer" component={AnswerScreen} />
+      {/* <Stack.Screen name="Schedule" component={ScheduleScreen} /> */}
+      {/* <Stack.Screen name="FlashCardQuestion" component={QuestionScreen} />
+      <Stack.Screen name="FlashCardAnswer" component={AnswerScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
