89301c62bef3489611d96a6421c31952cded1525
gerald1112 <50981837+gerald1112@users.noreply.github.com>
2022-07-27 22:20:20 +0800

    Re moved background of logo

--- leitner-frontend/src/screens/SplashScreen.js
+++ leitner-frontend/src/screens/SplashScreen.js
@@ -54,7 +54,7 @@ const SplashScreen = ({ navigation }) => {
 export default SplashScreen;
 
 const { height } = Dimensions.get('screen');
-const height_logo = height * 0.28;
+const height_logo = height * 0.45;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
